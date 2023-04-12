bb4c67f6550295e397cd3fb454bef6790a0f8d9e
Bullgamo9320 <ryom.1355@gmail.com>
2023-04-12 16:21:51 +0900

    Voting: テストの前処理実装

--- test/Voting.js
+++ test/Voting.js
@@ -2,12 +2,19 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Votingコントラクト", function() {
-    it("デプロイアドレスがownerに設定されるべき",async function(){
-        const Arr = ["Alice", "Bob"]
-        const [owner] = await ethers.getSigners();
-        const Voting = await ethers.getContractFactory("Voting");
-        const voting = await Voting.deploy(Arr);
+    let Voting;
+    let voting;
+    const Arr = ["Alice", "Bob"];
+    let owner;
+
+    beforeEach(async function() {
+        [owner] = await ethers.getSigners();
+        Voting = await ethers.getContractFactory("Voting");
+        voting = await Voting.deploy(Arr);
         await voting.deployed();
+    });
+
+    it("デプロイアドレスがownerに設定されるべき",async function(){
         expect(await voting.owner()).to.equal(owner.address);
     });
 })
