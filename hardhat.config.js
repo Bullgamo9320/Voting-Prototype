cd8782f7d212890ad2eda9fe57c9404c37620360
Bullgamo9320 <ryom.1355@gmail.com>
2023-04-20 15:16:38 +0900

    zkEVMでの実験。資金不足のため未完

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,8 +8,8 @@ require("@nomiclabs/hardhat-etherscan");
 module.exports = {
   solidity: "0.8.9",
   networks: {
-    mumbai: {
-      url:`https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+    zkevm: {
+      url:`https://polygonzkevm-testnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
       accounts: [process.env.PRIVATE_KEY],
     },
   },
--- scripts/Votingdeploy.js
+++ scripts/Votingdeploy.js
@@ -2,16 +2,13 @@ const fs = require("fs");
 const VotingAddress = require("../votingContract")
 
 const main = async () => {
-    //const addr1 = "0x2A4b1236d83a32b50210af38dcd197C68d268E32";
-    //const addr2 = "0xEfe71738025080CAd119BCC0AbcF399c41c7bD55";
-    //const addr3 = "0xF154A4d12adE3F9fbaC19f83A37D6D688c223507";
 
     // デプロイ
     Voting = await ethers.getContractFactory("Voting");
     voting = await Voting.deploy(["Alice", "Bob", "Chris"]);
     await voting.deployed();
 
-    console.log(`Contract deployed to: https://mumbai.polygonscan.com/address/${voting.address}`);
+    console.log(`Contract deployed to: https://testnet-zkevm.polygonscan.com/address/${voting.address}`);
 
     //voteする
     let tx = await voting.vote([100,100,100]);
